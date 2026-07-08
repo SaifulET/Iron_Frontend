b0c863c70b674ed06e62e46b99f3706d5eafdcb9
Saiful <si912999@gmail.com>
2026-07-08 06:40:29 +0600

    fix addon status toggle handle overlapping alignment

--- src/components/DashboardAddonsList.tsx
+++ src/components/DashboardAddonsList.tsx
@@ -101,9 +101,9 @@ export default function DashboardAddonsList() {
           </div>
           <button
             onClick={() => setAddonActive(!addonActive)}
-            className={`w-[38px] h-[21px] rounded-[11px] p-[3px] transition-colors duration-200 focus:outline-none flex items-center ${addonActive ? "bg-[#0F6E56]" : "bg-neutral-300"} cursor-pointer`}
+            className={`w-[38px] h-[21px] rounded-[11px] p-0.5 transition-colors duration-200 focus:outline-none flex items-center ${addonActive ? "bg-[#0F6E56]" : "bg-neutral-300"} cursor-pointer`}
           >
-            <div className={`w-[15px] h-[15px] bg-white rounded-full transition-transform duration-200 ${addonActive ? "translate-x-[17px]" : "translate-x-0"}`} />
+            <div className={`w-4 h-4 bg-white rounded-full transition-transform duration-200 shadow-sm ${addonActive ? "translate-x-[18px]" : "translate-x-0"}`} />
           </button>
         </div>
 
